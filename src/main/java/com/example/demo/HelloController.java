c99d6d11b1883cc5571599e010dcf9ff787655b7
chenyuan190622@163.com <chenyuan190622@163.com>
2018-12-17 23:34:44 +0800

    添加hello1方法和添加user类

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.dto.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +19,14 @@ public class HelloController {
     @RequestMapping("hello")
     public String hello(){
         return "hello git ";
+    }
+
+    @RequestMapping("hello1")
+    public String hello1(){
+        User user = new User();
+        user.setId(123);
+        user.setUsername("牛珪灵");
+        user.setPassword("123");
+        return "hello1 git"+user.getUsername();
     }
 }
---
+++ src/main/java/com/example/dto/User.java
@@ -0,0 +1,46 @@
+package com.example.dto;
+
+/**
+ * @author chenyuan
+ * @description {用一话描述该类}
+ * @date 2018/12/17
+ */
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
