dac266247d4c66dbc72ffd26990f80220d80c9bd
chenyuan190622@163.com <chenyuan190622@163.com>
2018-12-18 21:43:26 +0800

    添加UserDao

---
+++ src/main/java/com/example/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.example.dao;
+
+/**
+ * @author chenyuan
+ * @description {用一话描述该类}
+ * @date 2018/12/18
+ */
+
+
+public class UserDao {
+
+    public void selectUser(){
+        System.out.println("nihao");
+    }
+
+}
